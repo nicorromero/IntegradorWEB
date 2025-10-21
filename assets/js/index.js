b39106efd0bfe256d45df538110927ea2565632b
rodasjorgex <rodas15jorge@gmail.com>
2025-10-21 20:40:28 -0300

    hamburguesa v1

--- assets/js/index.js
+++ assets/js/index.js
@@ -43,6 +43,16 @@ function initIndex() {
 document.addEventListener("DOMContentLoaded", initIndex);
 
 
-   
+   const nav = document.querySelector('#nav');
+const abrir = document.querySelector('.abrir');
+const cerrar = document.querySelector('#cerrar');
+
+abrir.addEventListener('click', () => {
+    nav.classList.add('visible');
+});
+
+cerrar.addEventListener('click', () => {
+    nav.classList.remove('visible');
+});
 
 
